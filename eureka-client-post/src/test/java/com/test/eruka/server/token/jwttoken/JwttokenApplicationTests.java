a0cc103a7585b3801e5cb974f649449a75537874
Narsimha <snlshvsnj@gmail.com>
2019-08-04 18:28:29 +0530

    added mutoken, post,commnets servers

---
+++ eureka-client-post/src/test/java/com/test/eruka/server/token/jwttoken/JwttokenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.eruka.server.token.jwttoken;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwttokenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
